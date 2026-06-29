ac1e648a6d5952d32228465e0dea57ed2934e8d3
Danylo Perepeliuk <danyla958@gmail.com>
2026-06-30 01:10:26 +0300

    feat: update version code to 10 and revise README for kSteam integration details

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.orioooneee.lmuasister"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0.0"
     }
     packaging {
